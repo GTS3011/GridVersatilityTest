22cb60233d199331a3c5fae952df1a279a7f943d
GTS3011 <georgets3011@gmail.com>
2013-07-24 13:00:20 +0300

    first commit

---
+++ src/gr/epp/thesis/ViewItem.java
@@ -0,0 +1,16 @@
+package gr.epp.thesis;
+
+import javax.swing.Icon;
+
+/**
+ *
+ * @author vigos.ioannis
+ */
+public interface ViewItem {
+
+    public boolean isWarship();
+
+    public void itemIcon();
+
+    public String checker();
+}
